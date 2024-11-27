f0e8d7f3a1d65fb08d82e508be463d4667b82855
Anas Bashir <nasbas23@gmail.com>
2024-11-27 09:08:12 +0300

    Defined all the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+from django.db.models import (CASCADE, SET_NULL, BooleanField, CharField,
+                              DateField, DateTimeField, ForeignKey,
+                              ManyToManyField, Model, TextField)
 
-# Create your models here.
+
+class Author(Model):
+    name = CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(Model):
+    name = CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(Model):
+    title = CharField(max_length=255)
+    date_added = DateField()
+    allow_borrow = BooleanField(default=True)
+    author = ForeignKey(Author, on_delete=SET_NULL, null=True)
+    genres = ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title
+
+
+class Borrow(Model):
+    book = ForeignKey(Book, on_delete=CASCADE)
+    is_borrowed = BooleanField(default=True)
+    borrower_name = CharField(max_length=255)
+    borrowed_date = DateField()
+    returned_date = DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.borrower_name} - {self.book.title}"
+
+
+class Log(Model):
+    datetime = DateTimeField()
+    description = TextField()
+
+    def __str__(self):
+        return self.description
