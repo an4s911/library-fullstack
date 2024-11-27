e18d8ed223480217031e990be2c6018e7f5e8b60
Anas Bashir <nasbas23@gmail.com>
2024-11-27 23:09:52 +0300

    Add get-books/ url

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import index
+from .views import get_books
 
 urlpatterns = [
-    path("", index),
+    path("get-books/", get_books, name="get_books"),
 ]
