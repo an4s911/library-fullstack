5b38cae52499dd36a9c50bda6514b0125adafdc7
Anas Bashir <nasbas23@gmail.com>
2024-11-27 23:09:22 +0300

    Register models to admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Author, Book, Borrow, Genre, Log
+
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Log)
+admin.site.register(Borrow)
