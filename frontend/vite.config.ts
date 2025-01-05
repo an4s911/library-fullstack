fff6d409b0fd756c47f3e030b2351545112d002b
Anas Bashir <nasbas23@gmail.com>
2025-01-05 20:38:33 +0300

    Convert the frontend react app to a django compatible app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        base: "/static/",
+        plugins: [react()],
+        build: {
+            outDir: "dist/frontend",
+            emptyOutDir: true,
+            rollupOptions: {
+                input: "./src/main.tsx",
+                output: {
+                    entryFileNames: "main.js",
+                    chunkFileNames: "[name].js",
+                    assetFileNames: "[name].[ext]",
+                },
+            },
+            minify: "esbuild",
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                },
+                "/media": {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
 });
--- library/settings.py
+++ library/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "api",
+    "frontend",
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "frontend/dist",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
