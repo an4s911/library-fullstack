4b9fb769cb2662670b722eb32ba05d5a679c323c
Anas Bashir <nasbas23@gmail.com>
2025-01-07 00:56:53 +0300

    Done with the layout part of sort section

--- frontend/src/components/SearchFilter/SortSection.tsx
+++ frontend/src/components/SearchFilter/SortSection.tsx
@@ -1,7 +1,50 @@
+import { ArrowDownUpIcon } from "lucide-react";
+import { useState } from "react";
+import GenericSelect from "../UI/GenericSelect";
+
 type SortSectionProps = {};
 
 function SortSection({}: SortSectionProps) {
-    return <section className="sort-section">SortSection</section>;
+    const [sortBy, setSortBy] = useState("title");
+    const [isAscending, setIsAscending] = useState(true);
+
+    return (
+        <section className="sort-section flex items-center gap-2">
+            <ArrowDownUpIcon className="text-primary-400 dark:text-primary-500" />
+            <GenericSelect
+                value={sortBy}
+                onChange={(e) => setSortBy(e.target.value)}
+                optionsList={[
+                    {
+                        value: "title",
+                        label: "Title",
+                    },
+                    {
+                        value: "author",
+                        label: "Author",
+                    },
+                    {
+                        value: "date-added",
+                        label: "Date Added",
+                    },
+                ]}
+            />
+            <GenericSelect
+                value={isAscending ? "asc" : "desc"}
+                onChange={(e) => setIsAscending(e.target.value === "asc")}
+                optionsList={[
+                    {
+                        value: "asc",
+                        label: "Ascending",
+                    },
+                    {
+                        value: "desc",
+                        label: "Descending",
+                    },
+                ]}
+            />
+        </section>
+    );
 }
 
 export default SortSection;
