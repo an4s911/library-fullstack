3e4c58e5c203b90c98e47e12c6ec9998b8f57f65
Anas Bashir <nasbas23@gmail.com>
2025-01-06 16:13:57 +0300

    Remove some 'not yet necessary' stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(({}) => {
     return {
         base: "/static/",
         plugins: [react()],
@@ -23,18 +21,6 @@ export default defineConfig(({ mode }) => {
                 },
             },
             minify: "esbuild",
-        },
-        server: {
-            proxy: {
-                "/api": {
-                    target: env.VITE_API_URL,
-                    changeOrigin: true,
-                },
-                "/media": {
-                    target: env.VITE_API_URL,
-                    changeOrigin: true,
-                },
-            },
         },
     };
 });
