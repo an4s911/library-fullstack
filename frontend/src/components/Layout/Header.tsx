444f548044f28ba81d023589d7972e7b9fb6d82a
Anas Bashir <nasbas23@gmail.com>
2025-01-06 17:05:08 +0300

    Use a light bg color for dark mode header

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {};
 
 function Header({}: HeaderProps) {
     return (
-        <header className="flex justify-between items-center bg-white dark:bg-primary-900 sticky top-0 w-full shadow px-28 h-20 box-border py-4">
+        <header className="flex justify-between items-center bg-white dark:bg-primary-800 sticky top-0 w-full shadow px-20 h-20 box-border py-4">
             <div className="title-logo flex items-center gap-2">
                 <div className="logo text-primary dark:text-primary-200">
                     <Library size={32} />
