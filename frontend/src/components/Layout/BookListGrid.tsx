d740737a9527e214d4ff07649f5b4e6e4deacda5
Anas Bashir <nasbas23@gmail.com>
2025-01-09 21:02:52 +0300

    Add no books found message

--- frontend/src/components/Layout/BookListGrid.tsx
+++ frontend/src/components/Layout/BookListGrid.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { BookCard } from "../Book";
 import { BookListGridLoader } from "../SkeletonLoaders";
 import { Book } from "../../types/book";
+import { BookIcon } from "lucide-react";
 
 type BookListGridProps = {
     isLoading: boolean;
@@ -41,13 +42,21 @@ function BookListGrid({
     if (isLoading) {
         return <BookListGridLoader />;
     } else {
-        return (
+        return bookList.length > 0 ? (
             <div
                 className={`book-list-grid gap-5 ${isGrid ? "grid grid-cols-2 lg:grid-cols-3" : "flex flex-col"}`}
             >
                 {bookList.map((book) => {
                     return <BookCard isGrid={isGrid} book={book} key={book.id} />;
                 })}
+            </div>
+        ) : (
+            <div
+                className="w-full border border-primary-300 dark:border-primary-700
+                    rounded-md p-4 flex gap-3 justify-center items-center"
+            >
+                <BookIcon />
+                <h2 className="text-2xl">No books found</h2>
             </div>
         );
     }
