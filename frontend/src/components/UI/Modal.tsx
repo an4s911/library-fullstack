a386fef44c0cec3222ca6f6043dbf54dea58b196
Anas Bashir <nasbas23@gmail.com>
2025-01-09 13:58:14 +0300

    Make a generic modal with createPortal

--- frontend/src/components/UI/Modal.tsx
+++ frontend/src/components/UI/Modal.tsx
@@ -1,7 +1,23 @@
-type ModalProps = {};
+import { createPortal } from "react-dom";
 
-function Modal({}: ModalProps) {
-    return <div>Modal</div>;
+type ModalProps = {
+    children: React.ReactNode;
+    onClose: () => void;
+};
+
+function Modal({ children, onClose }: ModalProps) {
+    return createPortal(
+        <div className="h-screen w-screen absolute left-0 top-0 z-50">
+            <div
+                className="absolute w-full h-full bg-black opacity-50"
+                onClick={onClose}
+            />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                {children}
+            </div>
+        </div>,
+        document.getElementById("modal") as HTMLElement,
+    );
 }
 
 export default Modal;
