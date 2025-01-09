5daa1786775d458b2ff84cbd997ad1b6858219df
Anas Bashir <nasbas23@gmail.com>
2025-01-09 14:42:52 +0300

    Enable api url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({}) => {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
     return {
         base: "/static/",
         plugins: [react()],
@@ -21,6 +23,14 @@ export default defineConfig(({}) => {
                 },
             },
             minify: "esbuild",
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                },
+            },
         },
     };
 });
