18e410af3a778a5cb47e009266ed6a985a724f10
Anas Bashir <nasbas23@gmail.com>
2025-04-23 14:40:52 +0300

    Add constraints to Borrow model for data integrity
    Add Check and Unique constrains to the `Borrow` model to enforce data
    integrity:
    - Ensure `returned_date` is null for active borrows.
    - Prevent multiple active borrows for the same book.

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db.models import (CASCADE, SET_NULL, BooleanField, CharField,
-                              DateField, DateTimeField, ForeignKey,
-                              ManyToManyField, Model, TextField)
+                              CheckConstraint, DateField, DateTimeField,
+                              ForeignKey, ManyToManyField, Model, Q, TextField,
+                              UniqueConstraint)
 
 
 class Author(Model):
@@ -34,6 +35,27 @@ class Borrow(Model):
     borrower_name = CharField(max_length=255)
     borrowed_date = DateTimeField(auto_now_add=True)
     returned_date = DateTimeField(null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            CheckConstraint(
+                # This condition enforces that either:
+                # 1. The item is NOT borrowed (is_borrowed=False)
+                # OR
+                # 2. The returned_date is NULL (not set)
+                # It prevents the state where is_borrowed=True AND returned_date is
+                # NOT NULL.
+                check=Q(is_borrowed=False) | Q(returned_date__isnull=True),
+                # Choose a descriptive name for the constraint
+                name="borrow_returned_implies_not_borrowed_check",
+            ),
+            UniqueConstraint(
+                fields=["book"],  # Enforce uniqueness on the 'book' field
+                # Apply this constraint ONLY when 'is_borrowed' is True
+                condition=Q(is_borrowed=True),
+                name="borrow_unique_active_borrow_per_book",
+            ),
+        ]
 
     def __str__(self):
         return (
