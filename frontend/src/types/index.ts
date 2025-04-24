10314c4b5bba5bb5db6a39745241639e2a59408a
Anas Bashir <nasbas23@gmail.com>
2025-04-25 02:50:39 +0300

    Add Genre type

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,13 +1,19 @@
 export type Book = {
     id?: number;
     title: string;
-    author: number | null;
+    author: Author | null;
     dateAdded?: Date;
     allowBorrow: boolean;
     genres: string[];
+    borrowerName?: string;
 };
 
 export type Author = {
+    id: number;
+    name: string;
+};
+
+export type Genre = {
     id: number;
     name: string;
 };
