60083660d7c20f615062f6acd566c6b3ee95947b
Anas Bashir <nasbas23@gmail.com>
2025-04-24 18:07:43 +0300

    isGrid in Maincontent

--- frontend/src/components/Layout/MainContent.tsx
+++ frontend/src/components/Layout/MainContent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FilterSection, SortSection } from "../SearchFilter";
 import BookListGrid from "./BookListGrid";
 import LayoutToggleBtn from "./LayoutToggleBtn";
@@ -6,7 +6,13 @@ import LayoutToggleBtn from "./LayoutToggleBtn";
 type MainContentProps = {};
 
 function MainContent({}: MainContentProps) {
-    const [isGrid, setIsGrid] = useState(true);
+    const [isGrid, setIsGrid] = useState(
+        JSON.parse(localStorage.getItem("isGrid") || "true"),
+    );
+
+    useEffect(() => {
+        localStorage.setItem("isGrid", JSON.stringify(isGrid));
+    }, [isGrid]);
 
     return (
         <main
@@ -14,11 +20,14 @@ function MainContent({}: MainContentProps) {
                 display: "grid",
                 gridTemplateColumns: "250px 1fr",
             }}
-            className="px-20 py-8 w-full h-max gap-8 mt-20"
+            className="px-20 pb-8 w-full h-max gap-8 mt-20"
         >
             <FilterSection />
             <div className="flex flex-col gap-5">
-                <div className="w-full flex items-center justify-between sticky top-28 z-10">
+                <div
+                    className="w-full pt-8 pb-2 flex items-center
+                    justify-between sticky top-20 z-10"
+                >
                     <SortSection />
                     <LayoutToggleBtn
                         isGrid={isGrid}
