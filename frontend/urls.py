1c024eb6257d56b2a587213e6e2d5a58e66ea8e7
Anas Bashir <nasbas23@gmail.com>
2025-04-24 18:16:46 +0300

    Rename index in frontend to frontend_index

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
-    path("", views.index, name="index"),
+    path("", views.index, name="frontend_index"),
 ]
