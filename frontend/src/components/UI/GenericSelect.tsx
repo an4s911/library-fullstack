d04eb132888f6bdd9a2efaabcac640587fc4b7dc
Anas Bashir <nasbas23@gmail.com>
2025-04-25 02:51:00 +0300

    Update GenericSelect with more options

--- frontend/src/components/UI/GenericSelect.tsx
+++ frontend/src/components/UI/GenericSelect.tsx
@@ -1,29 +1,43 @@
 type OptionItem = {
-    value: string;
+    value: any;
     label: string;
 };
 
 type GenericSelectProps = {
     optionsList: OptionItem[];
-    value: string;
+    value: any;
     onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+    name?: string;
+    required?: boolean;
 };
 
-function GenericSelect({ optionsList, value, onChange }: GenericSelectProps) {
+function GenericSelect({
+    optionsList,
+    value,
+    onChange,
+    name,
+    required = false,
+}: GenericSelectProps) {
     return (
         <select
-            value={value}
+            name={name}
+            value={value === -1 ? "" : value}
             onChange={onChange}
-            className="px-3 py-2 rounded-lg
+            className="px-3 py-2 rounded-lg w-full
                     focus:outline-none focus:ring-2
                     focus:ring-primary-400 dark:focus:ring-primary-500
                     bg-primary-200 dark:bg-primary-700
-                    border border-primary-700 dark:border-primary-600
-                "
+                    border border-primary-700 dark:border-primary-600"
+            required={required}
         >
             {optionsList.map((option) => {
                 return (
-                    <option key={option.value} value={option.value}>
+                    <option
+                        key={option.value}
+                        value={option.value === -1 ? "" : option.value}
+                        disabled={option.value === -1}
+                        defaultChecked
+                    >
                         {option.label}
                     </option>
                 );
