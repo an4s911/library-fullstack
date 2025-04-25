78dfa6ec3befbe35d9567a21d5a09903c0801be6
Anas Bashir <nasbas23@gmail.com>
2025-04-26 00:39:26 +0300

    Add disabled option in GenericButton

--- frontend/src/components/UI/GenericButton.tsx
+++ frontend/src/components/UI/GenericButton.tsx
@@ -1,14 +1,16 @@
 type GenericButtonProps = {
     children: React.ReactNode;
     onClick?: () => void;
+    disabled?: boolean;
 };
 
-function GenericButton({ children, onClick }: GenericButtonProps) {
+function GenericButton({ children, onClick, disabled = false }: GenericButtonProps) {
     return (
         <button
             onClick={onClick}
             className="px-3 py-2 bg-primary text-white hover:bg-primary-500 dark:hover:bg-primary-400
             rounded-md flex gap-2 items-center"
+            disabled={disabled}
         >
             {children}
         </button>
