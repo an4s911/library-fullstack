5a9d83e30c2591b3cf499b809c07b93087e672de
Anas Bashir <nasbas23@gmail.com>
2025-04-26 00:38:30 +0300

    Make Book.id not optional and add an option to get date with time as well

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,12 +1,12 @@
 export type Book = {
-    id?: number;
+    id: number;
     title: string;
     author: Author | null;
     dateAdded: Date;
     allowBorrow: boolean;
     genres: string[];
     borrowerName?: string;
-    getDateAdded: () => string;
+    getDateAdded: (withTime?: boolean) => string;
 };
 
 export type Author = {
@@ -21,11 +21,12 @@ export type Genre = {
 
 export function createBook(book: Book): Book {
     const dateAdded = new Date(book.dateAdded);
-    const getDateAdded = () => {
+    const getDateAdded = (withTime: boolean = false) => {
         return dateAdded.toLocaleDateString("en-US", {
             month: "short",
             day: "numeric",
             year: "numeric",
+            ...(withTime && { hour: "numeric", minute: "numeric" }),
         });
     };
 
