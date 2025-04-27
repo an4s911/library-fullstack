d4929b7e507af6e32a949a633ccc75204330c1b9
Anas Bashir <nasbas23@gmail.com>
2025-04-28 00:21:20 +0300

    Add a public/index.html for hosting the frontend alone
    - Also set the title automatically

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,13 @@ import { Header } from "@/components/Layout";
 import { HomePage } from "@/pages";
 import { OptionsProvider, PageContextProvider } from "@/contexts";
 import { ModalProvider } from "@/contexts";
+import { useEffect } from "react";
 
 function App() {
+    useEffect(() => {
+        document.title = import.meta.env.VITE_APP_NAME;
+    }, []);
+
     return (
         <div
             style={{
