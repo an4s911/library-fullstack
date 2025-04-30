1a6fd83f77a850e55c26c2c3c8e5ad039c1d0797
Anas Bashir <nasbas23@gmail.com>
2025-04-30 15:14:13 +0300

    Separate the utils each into their own separate modules and use index.ts for exporting

---
+++ frontend/src/utils/fetchWithToast.ts
@@ -0,0 +1,33 @@
+import { toast } from "react-toastify";
+
+type FetchWithToastOptions = {
+    okCallback?: () => void;
+    dataCallback?: (data: any) => void;
+    showToast?: boolean;
+};
+
+const fetchWithToast = async (
+    input: RequestInfo | URL,
+    init?: RequestInit,
+    { okCallback, dataCallback, showToast = true }: FetchWithToastOptions = {},
+) => {
+    fetch(input, init)
+        .then((res) => {
+            if (res.ok) {
+                okCallback && okCallback();
+            }
+            return res.json();
+        })
+        .then((data) => {
+            if (showToast) {
+                if (data.error) {
+                    toast.error(data.error);
+                } else if (data.message) {
+                    toast.success(data.message);
+                }
+            }
+            dataCallback && dataCallback(data);
+        });
+};
+
+export default fetchWithToast;
